bdaeb500fe1822d4d23b95460eb0e823985607b1
Alex Sasov <sasov1993@mail.ru>
2022-12-10 13:33:13 +0300

    знакомство с imageSharp https://habr.com/ru/post/482274/

---
+++ ImageSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Image image = Image.Load("Examples/img.jpg");
+
+var txt = "test";
+image.Mutate(img =>
+{
+    var ff = image.Metadata.HorizontalResolution / ImageMetadata.DefaultHorizontalResolution;
+    var font = SystemFonts.CreateFont("Arial", (float)(24 * Math.Max(ff, 1)), FontStyle.Regular);
+    var size = TextMeasurer.Measure(txt, new TextOptions(font));
+    float scalingFactor = image.Height / size.Height / 10;
+    var scalingFont = new Font(font, scalingFactor * font.Size);
+    img.DrawText(txt, scalingFont, Color.Black, new PointF(image.Width / 2 - 50, image.Height / 2 - 50) );
+});
+
+image.SaveAsJpeg("test.jpg");
+
+Console.WriteLine("Hello, World!");
+
+
+
---
+++ ImageSharp/globalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using SixLabors.Fonts;
+global using SixLabors.ImageSharp;
+global using SixLabors.ImageSharp.Metadata;
+global using SixLabors.ImageSharp.Processing;
+global using SixLabors.ImageSharp.Drawing.Processing;
